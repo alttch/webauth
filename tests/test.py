58d955d1f742f96a16c94ea931133f20d2b12433
Sergei S <div@altertech.com>
2020-04-07 23:44:07 +0200

    github auth fix

--- tests/test.py
+++ tests/test.py
@@ -77,9 +77,9 @@ def click(elem):
     _d.driver.find_element_by_id(elem).click()
     if elem.endswith('-github'):
         try:
-            time.sleep(0.1)
             click('js-oauth-authorize-btn')
             time.sleep(0.5)
+            click('js-oauth-authorize-btn')
         except NoSuchElementException:
             pass
 
