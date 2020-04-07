6932ce98b09ab6fadce51146209eef061dd1cb0d
Sergei S <div@altertech.com>
2020-04-07 17:54:27 +0200

    auto test for register

--- example/test.py
+++ example/test.py
@@ -3,17 +3,24 @@
 import pytest
 import time
 import os
+import re
 import signal
 import logging
+import email
+from poplib import POP3
 from pyaltt2.db import Database
+from pyaltt2.config import load_yaml
 from types import SimpleNamespace
 from selenium import webdriver
 from selenium.webdriver.firefox.options import Options
+from selenium.common.exceptions import NoSuchElementException
 
 pidfile = '/tmp/webauth-server-test.pid'
 logfile = '/tmp/webauth-server-test.log'
 
 db = Database('postgresql://test:123@localhost/test')
+
+config = load_yaml('test-data.yml')
 
 _d = SimpleNamespace()
 
@@ -32,7 +39,8 @@ def init():
         os.unlink(logfile)
     except:
         pass
-    if os.system('gunicorn -D -b 0.0.0.0:8449 --log-level DEBUG '
+    if os.system('WEBAUTH_CONFIG=test.yml '
+                 'gunicorn -D -b 0.0.0.0:8449 --log-level DEBUG '
                  f'--pid {pidfile} --log-file {logfile} server:app'):
         raise RuntimeError('failed to start server')
     c = 0
@@ -44,6 +52,12 @@ def init():
         c += 1
         time.sleep(0.1)
         if c > 50: raise TimeoutError
+    d = _d.driver
+    # github login
+    # d.get('https://github.com/login')
+    # d.find_element_by_id('login_field').send_keys(config['github']['login'])
+    # d.find_element_by_id('password').send_keys(config['github']['password'])
+    # d.find_element_by_class_name('btn-primary').click()
     yield
     _d.driver.quit()
     with open(pidfile) as fh:
@@ -55,6 +69,64 @@ def init():
         pass
 
 
-def test1():
-    _d.driver.get('https://webauth-test.lab.altt.ch/')
-    time.sleep(2)
+def _login_pop3(wait_mail=None):
+    pop3 = POP3(host=config['pop3']['host'])
+    pop3.user(config['pop3']['login'])
+    pop3.pass_(config['pop3']['password'])
+    if wait_mail:
+        for i in range(wait_mail * 10):
+            if len(pop3.list()[1]):
+                break
+            time.sleep(0.1)
+        else:
+            raise TimeoutError
+    return pop3
+
+
+def _get_pop3_mail():
+    pop3 = _login_pop3(wait_mail=5)
+    msg = email.message_from_string('\n'.join(
+        [x.decode() for x in pop3.retr(1)[1]]))
+    pop3.dele(1)
+    pop3.quit()
+    return msg
+
+
+def _get_pop3_link():
+    payload = str(_get_pop3_mail().get_payload()[0])
+    return re.search('(?P<url>https?://[^\s]+)', payload).group('url')
+
+
+def _clear_pop3():
+    pop3 = _login_pop3()
+    for i in range(len(pop3.list()[1])):
+        pop3.dele(i + 1)
+    pop3.quit()
+
+
+def _test001_oauth_login():
+    d = _d.driver
+    d.get('https://webauth-test.lab.altt.ch/')
+    d.find_element_by_id('login-github').click()
+    d.find_element_by_id('delete-account').click()
+    assert d.current_url == 'https://webauth-test.lab.altt.ch/'
+
+
+def test002_register():
+    _clear_pop3()
+    d = _d.driver
+    d.get('https://webauth-test.lab.altt.ch/')
+    d.find_element_by_id('do_reg').click()
+    d.find_element_by_id('email_reg').send_keys(config['email'][0])
+    d.find_element_by_id('password').send_keys('123')
+    d.find_element_by_id('password2').send_keys('123')
+    d.find_element_by_id('reg_submit').click()
+    assert d.current_url == 'https://webauth-test.lab.altt.ch/dashboard'
+    assert d.find_element_by_id('resend-confirm') is not None
+    _clear_pop3()
+    d.find_element_by_id('resend-confirm').click()
+    d.get(_get_pop3_link())
+    assert d.current_url == 'https://webauth-test.lab.altt.ch/dashboard'
+    with pytest.raises(NoSuchElementException):
+        d.find_element_by_id('resend-confirm')
+    d.find_element_by_id('delete-account').click()
