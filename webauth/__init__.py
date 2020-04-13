18111cb9c1dd4459fce0daf4b377d022f7fe9638
Sergei S <div@altertech.com>
2020-04-13 16:32:14 +0200

    0.0.14

--- webauth/__init__.py
+++ webauth/__init__.py
@@ -1,7 +1,7 @@
 __author__ = 'Altertech, https://www.altertech.com/'
 __copyright__ = 'Copyright (C) 2020 Altertech'
 __license__ = 'MIT'
-__version__ = '0.0.13'
+__version__ = '0.0.14'
 
 # TODO: 2fa
 
